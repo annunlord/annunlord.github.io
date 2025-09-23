32298181e8a0f3980d4ab0eddaabd403b5824739
William Sciaroni <wsciaroni@gmail.com>
2025-09-23 20:42:02 +0000

    Add animation delay in test

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -2,5 +2,6 @@ const { test, expect } = require('@playwright/test');
 
 test('visual regression test for home page', async ({ page }) => {
   await page.goto('/');
+  await page.waitForTimeout(1100); // Animation delay is 0.25s and animation is 0.3s. So, 1100ms is double the expected time
   await expect(page).toHaveScreenshot();
 });
