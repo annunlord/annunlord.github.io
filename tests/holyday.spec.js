594ea7ac8eab58e3fd511f30782e5ed1fdb1e0fb
William Sciaroni <wsciaroni@gmail.com>
2025-09-23 18:41:11 -0500

    Update test to correctly get the time into the page

--- tests/holyday.spec.js
+++ tests/holyday.spec.js
@@ -1,22 +1,26 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Holy Day Mass Times', () => {
-  // Set the date to May 17, 2025
-  test.use({
-    contextOptions: {
-      now: new Date('2025-05-17T12:00:00Z').getTime(),
-    },
-  });
+  // The date to be mocked in the browser
+  const mockedDate = new Date('2025-08-14T12:00:00Z');
 
   test('home page should show holy day banner', async ({ page }) => {
+    // Mock the date before navigating to the page
+    await page.clock.setFixedTime(mockedDate);
+
     await page.goto('/');
-    await page.waitForTimeout(1100);
+    // You may not need this timeout if the script runs quickly on DOMContentLoaded
+    await page.waitForTimeout(1100); 
+    await expect(page.locator('#holyday-banner-alert')).toBeVisible();
     await expect(page).toHaveScreenshot({ fullPage: true });
   });
 
   test('mass times page should show holy day schedule', async ({ page }) => {
+    // Mock the date before navigating to the page
+    await page.clock.setFixedTime(mockedDate);
+    
     await page.goto('/information/mass_times/');
-    await page.waitForTimeout(1100);
+    await expect(page.locator('#holyday-schedule-card')).toBeVisible();
     await expect(page).toHaveScreenshot({ fullPage: true });
   });
 });
